a69a46b48bc474ba66ebb8f60edec51f5aa1e018
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-07-11 21:14:23 +0800

    output json file.

--- main.cpp
+++ main.cpp
@@ -7,130 +7,23 @@
 #include <map>
 #include <glslang/ShaderLang.h>
 #include <glslang/Types.h>
+#include <json.hpp>
 #include "init_resources.h"
+#include "gl2vulkan.h"
+#include "config.h"
 
-class Config {
-public:
-	enum Option {
-		DEF,
-		STAGE
-	};
-	enum OptionValue {
-		UNKNOWN,
-		OPENGL,
-		VULKAN,
-		VERTEX,
-		FRAGMENT
-	};
+using JSON = nlohmann::json;
 
-	Config() = delete;
-	
-	Config(int argc, char** argv) {
-		if (argc == 1) {
-			throw std::runtime_error("input file should be set.");
-		}
-		std::vector<std::string> arguments(argc - 1);
-		int minus_count = 0;
-
-		for (int i = 1; i < argc; ++i) { // ignore the exe filename.
-			arguments[i - 1] = argv[i];
-			++minus_count;
-		}
-
-		if(minus_count > 0) {
-			setOption(arguments);
-		}
-
-		if (isUnset(m_input)) {
-			for (const auto& arg : arguments) {
-				if (arg[0] != '-' && arg.size() > 0) {
-					m_input = arg;
-					break;
-				}
-			}
-			if (isUnset(m_input))
-				throw std::runtime_error("input file should be set.");
-		}
-
-		if (isUnset(DEF)) {
-			m_options[DEF] = OPENGL;
-		}
-
-		if (isUnset(STAGE)) {
-			throw std::runtime_error("stage should be set.");
-		}
-	}
-
-	~Config() {
-	
-	};
-
-	bool isVulkanDef() const { 
-		if(m_options.count(DEF) > 0)
-			return m_options.find(DEF)->second == VULKAN; 
-		else
-			throw std::runtime_error("stage should be set.");
-		return false;
-	}
-
-	OptionValue getStage() const { 
-		if (m_options.count(STAGE) > 0)
-			return m_options.find(STAGE)->second;
-		else 
-			throw std::runtime_error("stage should be set.");
-		return UNKNOWN;
-	}
-	std::string getInputFilename() const { return m_input; }
-	std::string getOutputFilename() const { return m_output; }
-
-private:
-	void setOption(const std::vector<std::string>& arguments) {
-		auto size = arguments.size();
-		for (int i = 0; i < size;) {
-			if (arguments[i][0] == '-' && arguments[i].size() == 2) {
-				char option = arguments[i][1];
-				const auto& argument = arguments[i + 1];
-				if (option == 'o') { // OUTPUT FILE
-					m_output = argument;
-				}
-				else if (option == 'i') { // INPUT FILE
-					m_input = argument;
-				}
-				else if (option == 's') { // STAGE
-					if (argument == "fragment") {
-						m_options[STAGE] = FRAGMENT;
-					}
-					else if (argument == "vertex") {
-						m_options[STAGE] = VERTEX;
-					}
-				}
-				else if (option == 'v') { // IS VULKAN
-					if (argument == "true") {
-						m_options[DEF] = VULKAN;
-					}
-					else {
-						m_options[DEF] = OPENGL;
-					}
-				}
-				i += 2;
-			}
-			else
-				++i;
-		}
-	}
-
-	bool isUnset(const std::string& str) const {
-		return str.empty();
-	}
-
-	bool isUnset(const Option option) const {
-		return m_options.count(option) == 0;
-	}
-
-	std::map<Option, OptionValue> m_options;
-	std::string m_input;
-	std::string m_output;
-};
+void SetQualifier(const glslang::TQualifier& qualifier, JSON& json) {
+	if (qualifier.hasBinding())
+		json["binding"] = qualifier.layoutBinding;
+	if (qualifier.hasLocation())
+		json["location"] = qualifier.layoutLocation;
+	if (qualifier.layoutPushConstant)
+		json["push_constant"] = "true";
+	else
+		json["push_constant"] = "false";
+}
 
 int main(int argc, char** argv) {
 	try {
@@ -197,46 +90,55 @@ int main(int argc, char** argv) {
 			return 1;
 		}
 
-		std::cout << "---------------------------------------------" << std::endl;
-		std::cout << "Attributes:" << std::endl;
+		JSON conf_json;
+
+		JSON attributes_json;
 		auto attributes_size = program.getNumLiveAttributes();
 		for (int i = 0; i < attributes_size; ++i) {
-			std::cout << program.getAttributeName(i) << std::endl;
-			std::cout << program.getAttributeType(i) << std::endl;
+			JSON attri_json;
+			auto attribute_type = program.getAttributeType(i);
+			attri_json["type"] = config.isVulkanDef() ? AttributeGL2Vulkan(attribute_type) : attribute_type;
+			
 			const auto& type = program.getAttributeTType(i);
-			std::cout << type->getVectorSize() << std::endl;
-			const auto& qualifier = type->getQualifier();
-			if (qualifier.hasBinding())
-				std::cout << "binding: " << qualifier.layoutBinding << std::endl;
-			if (qualifier.hasLocation())
-				std::cout << "location: " << qualifier.layoutLocation << std::endl;
-			if (qualifier.layoutPushConstant)
-				std::cout << "PushConstant" << std::endl;
-			std::cout << std::endl;
-		}
+			attri_json["vector_size"] = type->getVectorSize();
 
-		std::cout << "---------------------------------------------" << std::endl;
-		std::cout << "UniformBlocks:" << std::endl;
+			const auto& qualifier = type->getQualifier();
+			SetQualifier(qualifier, attri_json);
+
+			attributes_json[program.getAttributeName(i)] = attri_json;
+		}
+		conf_json["attributes"] = attributes_json;
+
+		JSON uniform_blocks_json;
 		auto uniforms_size = program.getNumLiveUniformBlocks();
 		for (int i = 0; i < uniforms_size; ++i) {
-			std::cout << program.getUniformBlockName(i) << std::endl;
-			std::cout << "block size:" << program.getUniformBlockSize(i) << std::endl;
+			JSON uniform_blk_json;
+			uniform_blk_json["block_size"] = program.getUniformBlockSize(i);
+			
 			const auto& type = program.getUniformBlockTType(i);
+
 			const auto& qualifier = type->getQualifier();
-			if (qualifier.hasBinding())
-				std::cout << "binding: " << qualifier.layoutBinding << std::endl;
-			if (qualifier.hasLocation())
-				std::cout << "location: " << qualifier.layoutLocation << std::endl;
-			if (qualifier.layoutPushConstant)
-				std::cout << "PushConstant" << std::endl;
-			std::cout << std::endl;
+			SetQualifier(qualifier, uniform_blk_json);
+
+			uniform_blocks_json[program.getUniformBlockName(i)] = uniform_blk_json;
 		}
+		conf_json["uniform_blocks"] = uniform_blocks_json;
 
 		glslang::FinalizeProcess();
+
+		std::ofstream output_file(config.getOutputFilename());
+		if(!output_file.is_open()) {
+			std::cout
+				<< "Something is going wrong,"
+				<< "file can not be loaded!"
+				<< std::endl;
+			return 1;
+		}
+		output_file << std::setw(4) << conf_json;
 	}
 	catch(std::runtime_error& error) {
 		std::cout << error.what() << std::endl;
-		std::cout << "ShaderRetriever <filename> <vertex|fragment> <Output Filename>" << std::endl;
+		std::cout << "ShaderRetriever -i <filename> -s <vertex|fragment> -o <Output Filename> -v <true|false>" << std::endl;
 		return 1;
 	}
 	return 0;
---
+++ src/config.h
@@ -0,0 +1,128 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <exception>
+#include <map>
+
+class Config {
+public:
+	enum Option {
+		DEF,
+		STAGE
+	};
+	enum OptionValue {
+		UNKNOWN,
+		OPENGL,
+		VULKAN,
+		VERTEX,
+		FRAGMENT
+	};
+
+	Config() = delete;
+
+	Config(int argc, char** argv) {
+		if (argc == 1) {
+			throw std::runtime_error("input file should be set.");
+		}
+		std::vector<std::string> arguments(argc - 1);
+		int minus_count = 0;
+
+		for (int i = 1; i < argc; ++i) { // ignore the exe filename.
+			arguments[i - 1] = argv[i];
+			++minus_count;
+		}
+
+		if (minus_count > 0) {
+			setOption(arguments);
+		}
+
+		if (isUnset(m_input)) {
+			for (const auto& arg : arguments) {
+				if (arg[0] != '-' && arg.size() > 0) {
+					m_input = arg;
+					break;
+				}
+			}
+			if (isUnset(m_input))
+				throw std::runtime_error("input file should be set.");
+		}
+
+		if (isUnset(DEF)) {
+			m_options[DEF] = OPENGL;
+		}
+
+		if (isUnset(STAGE)) {
+			throw std::runtime_error("stage should be set.");
+		}
+	}
+
+	~Config() {
+
+	};
+
+	bool isVulkanDef() const {
+		if (m_options.count(DEF) > 0)
+			return m_options.find(DEF)->second == VULKAN;
+		else
+			throw std::runtime_error("stage should be set.");
+		return false;
+	}
+
+	OptionValue getStage() const {
+		if (m_options.count(STAGE) > 0)
+			return m_options.find(STAGE)->second;
+		else
+			throw std::runtime_error("stage should be set.");
+		return UNKNOWN;
+	}
+	std::string getInputFilename() const { return m_input; }
+	std::string getOutputFilename() const { return m_output; }
+
+private:
+	void setOption(const std::vector<std::string>& arguments) {
+		auto size = arguments.size();
+		for (int i = 0; i < size;) {
+			if (arguments[i][0] == '-' && arguments[i].size() == 2) {
+				char option = arguments[i][1];
+				const auto& argument = arguments[i + 1];
+				if (option == 'o') { // OUTPUT FILE
+					m_output = argument;
+				}
+				else if (option == 'i') { // INPUT FILE
+					m_input = argument;
+				}
+				else if (option == 's') { // STAGE
+					if (argument == "fragment") {
+						m_options[STAGE] = FRAGMENT;
+					}
+					else if (argument == "vertex") {
+						m_options[STAGE] = VERTEX;
+					}
+				}
+				else if (option == 'v') { // IS VULKAN
+					if (argument == "true") {
+						m_options[DEF] = VULKAN;
+					}
+					else {
+						m_options[DEF] = OPENGL;
+					}
+				}
+				i += 2;
+			}
+			else
+				++i;
+		}
+	}
+
+	bool isUnset(const std::string& str) const {
+		return str.empty();
+	}
+
+	bool isUnset(const Option option) const {
+		return m_options.count(option) == 0;
+	}
+
+	std::map<Option, OptionValue> m_options;
+	std::string m_input;
+	std::string m_output;
+};
